61822014835c29820274e6ebf09a05b2e41c7d71
72565 <dferrarini@dferrarini-VirtualBox>
2017-10-04 15:24:10 +0200

    commentata import lib vsphere

--- beehive/manager/manage.py
+++ beehive/manager/manage.py
@@ -20,7 +20,7 @@ from beehive.manager.sections.catalog import catalog_controller_handlers
 from beehive.manager.sections.event import event_controller_handlers
 from beehive.manager.sections.scheduler import scheduler_controller_handlers
 from beehive.manager.sections.service import service_controller_handlers
-from beehive.manager.sections.vsphere import vsphere_controller_handlers
+# from beehive.manager.sections.vsphere import vsphere_controller_handlers
 from beehive.manager.sections.openstack import openstack_controller_handlers,\
     openstack_platform_controller_handlers
 logging.captureWarnings(True)
