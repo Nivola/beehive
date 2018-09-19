c23df99af533294d1a8de74a69b41ee93a62c330
Antonio <a.brasile@tdnet.it>
2018-09-19 17:12:38 +0200

    bugs su scheduler on version celery 4.2.1

--- beehive/module/scheduler/redis_scheduler.py
+++ beehive/module/scheduler/redis_scheduler.py
@@ -8,102 +8,103 @@ from beecell.db.manager import RedisManager
 from celery.beat import Scheduler
 from datetime import timedelta
 from celery.schedules import maybe_schedule, crontab, schedule as interval
+from celery.beat import ScheduleEntry 
 from celery.five import items
 from kombu.utils import reprcall
 import redis_collections
 from beecell.simple import format_date
 #import pickle
 
-class RedisScheduleEntry(object):
-    """An entry in the scheduler.
-
-    :keyword name: see :attr:`name`.
-    :keyword schedule: see :attr:`schedule`.
-    :keyword args: see :attr:`args`.
-    :keyword kwargs: see :attr:`kwargs`.
-    :keyword options: see :attr:`options`.
-    :keyword last_run_at: see :attr:`last_run_at`.
-    :keyword total_run_count: see :attr:`total_run_count`.
-    :keyword relative: Is the time relative to when the server starts?
-
-    """
-
-    #: The task name
-    name = None
-
-    #: The schedule (run_every/crontab)
-    schedule = None
-
-    #: Positional arguments to apply.
-    args = None
-
-    #: Keyword arguments to apply.
-    kwargs = None
-
-    #: Task execution options.
-    options = None
-
-    #: The time and date of when this task was last scheduled.
-    last_run_at = None
-
-    #: Total number of times this task has been scheduled.
-    total_run_count = 0
-
-    def __init__(self, name=None, task=None, last_run_at=None,
-                 total_run_count=None, schedule=None, args=(), kwargs={},
-                 options={}, relative=False, app=None):
-        self.app = app
-        self.name = name
-        self.task = task
-        self.args = args
-        self.kwargs = kwargs
-        self.options = options
-        self.schedule = maybe_schedule(schedule, relative, app=self.app)
-        self.last_run_at = last_run_at or self._default_now()
-        self.total_run_count = total_run_count or 0
-
-    def _default_now(self):
-        return self.schedule.now() if self.schedule else self.app.now()
-
-    def _next_instance(self, last_run_at=None):
-        """Return a new instance of the same class, but with
-        its date and count fields updated."""
-        return self.__class__(**dict(
-            self,
-            last_run_at=last_run_at or self._default_now(),
-            total_run_count=self.total_run_count + 1,
-        ))
-    __next__ = next = _next_instance  # for 2to3
-
-    def __reduce__(self):
-        return self.__class__, (
-            self.name, self.task, self.last_run_at, self.total_run_count,
-            self.schedule, self.args, self.kwargs, self.options,
-        )
-
-    def update(self, other):
-        """Update values from another entry.
-
-        Does only update "editable" fields (task, schedule, args, kwargs,
-        options).
-
-        """
-        self.__dict__.update({'task': other.task, 'schedule': other.schedule,
-                              'args': other.args, 'kwargs': other.kwargs,
-                              'options': other.options})
-
-    def is_due(self):
-        """See :meth:`~celery.schedule.schedule.is_due`."""
-        return self.schedule.is_due(self.last_run_at)
-
-    def __iter__(self):
-        return iter(items(vars(self)))
-
-    def __repr__(self):
-        return '<Entry: {0.name} {call} {0.schedule}'.format(
-            self,
-            call=reprcall(self.task, self.args or (), self.kwargs or {}),
-        )
+class RedisScheduleEntry(ScheduleEntry):
+#     """An entry in the scheduler.
+#  
+#     :keyword name: see :attr:`name`.
+#     :keyword schedule: see :attr:`schedule`.
+#     :keyword args: see :attr:`args`.
+#     :keyword kwargs: see :attr:`kwargs`.
+#     :keyword options: see :attr:`options`.
+#     :keyword last_run_at: see :attr:`last_run_at`.
+#     :keyword total_run_count: see :attr:`total_run_count`.
+#     :keyword relative: Is the time relative to when the server starts?
+#  
+#     """
+#  
+#     #: The task name
+#     name = None
+#  
+#     #: The schedule (run_every/crontab)
+#     schedule = None
+#  
+#     #: Positional arguments to apply.
+#     args = None
+#  
+#     #: Keyword arguments to apply.
+#     kwargs = None
+#  
+#     #: Task execution options.
+#     options = None
+#  
+#     #: The time and date of when this task was last scheduled.
+#     last_run_at = None
+#  
+#     #: Total number of times this task has been scheduled.
+#     total_run_count = 0
+#  
+#     def __init__(self, name=None, task=None, last_run_at=None,
+#                  total_run_count=None, schedule=None, args=(), kwargs={},
+#                  options={}, relative=False, app=None):
+#         self.app = app
+#         self.name = name
+#         self.task = task
+#         self.args = args
+#         self.kwargs = kwargs
+#         self.options = options
+#         self.schedule = maybe_schedule(schedule, relative, app=self.app)
+#         self.last_run_at = last_run_at or self.default_now()
+#         self.total_run_count = total_run_count or 0
+#  
+#     def default_now(self):
+#         return self.schedule.now() if self.schedule else self.app.now()
+#  
+#     def _next_instance(self, last_run_at=None):
+#         """Return a new instance of the same class, but with
+#         its date and count fields updated."""
+#         return self.__class__(**dict(
+#             self,
+#             last_run_at=last_run_at or self._default_now(),
+#             total_run_count=self.total_run_count + 1,
+#         ))
+#     __next__ = next = _next_instance  # for 2to3
+#  
+#     def __reduce__(self):
+#         return self.__class__, (
+#             self.name, self.task, self.last_run_at, self.total_run_count,
+#             self.schedule, self.args, self.kwargs, self.options,
+#         )
+#  
+#     def update(self, other):
+#         """Update values from another entry.
+#  
+#         Does only update "editable" fields (task, schedule, args, kwargs,
+#         options).
+#  
+#         """
+#         self.__dict__.update({'task': other.task, 'schedule': other.schedule,
+#                               'args': other.args, 'kwargs': other.kwargs,
+#                               'options': other.options})
+#  
+#     def is_due(self):
+#         """See :meth:`~celery.schedule.schedule.is_due`."""
+#         return self.schedule.is_due(self.last_run_at)
+#  
+#     def __iter__(self):
+#         return iter(items(vars(self)))
+#  
+#     def __repr__(self):
+#         return '<Entry: {0.name} {call} {0.schedule}'.format(
+#             self,
+#             call=reprcall(self.task, self.args or (), self.kwargs or {}),
+#         )
         
     def info(self):
         """ """
