f4e1351047e4a9e3a47bb926853072e0a02f1f59
1362 <sergio.tonani@csi.it>
2019-02-07 17:12:59 +0100

    aggiornati test per fare test concorrenti

--- beehive/common/test.py
+++ beehive/common/test.py
@@ -115,13 +115,13 @@ class BeehiveTestCase(unittest.TestCase):
 
         logger.info(u'Validation active: %s' % cls.validation_active)
 
-        print(u'Configurations:')
-        print(u'Main config file: %s' % cls.main_config_file)
-        print(u'Extra config file: %s' % cls.spec_config_file)
-        print(u'Validation active: %s' % cls.validation_active)
-        print(u'Test user: %s' % cls.run_test_user)
-        print(u'')
-        print(u'Tests:')
+        # print(u'Configurations:')
+        # print(u'Main config file: %s' % cls.main_config_file)
+        # print(u'Extra config file: %s' % cls.spec_config_file)
+        # print(u'Validation active: %s' % cls.validation_active)
+        # print(u'Test user: %s' % cls.run_test_user)
+        # print(u'')
+        # print(u'Tests:')
 
         # env = config.get(u'env', None)
         # if env is None:
@@ -612,3 +612,258 @@ def runtest(testcase_class, tests, args={}):
     # run test suite
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(unittest.TestSuite(map(testcase_class, tests)))
+
+
+def runtest_parallel(testcase_class, tests, args={}):
+    """Run test. Accept as external input args:
+    -
+        main_config_file = None
+    spec_config_file = None
+    validation_active
+
+
+    :param testcase_class:
+    :param tests:
+    :return:
+    """
+    home = os.path.expanduser(u'~')
+    log_file = home + u'/test.log'
+    watch_file = home + u'/test.watch'
+    run_file = home + u'/test.run'
+
+    logging.captureWarnings(True)
+
+    loggers = [
+        logging.getLogger(u'beecell.perf'),
+    ]
+    LoggerHelper.file_handler(loggers, logging.DEBUG, watch_file, frmt=u'%(message)s', formatter=ColorFormatter)
+
+    loggers = [
+        logging.getLogger(u'beehive.test.run'),
+    ]
+    LoggerHelper.file_handler(loggers, logging.DEBUG, run_file, frmt=u'%(message)s', formatter=ColorFormatter)
+
+    # setting logger
+    # frmt = "%(asctime)s - %(levelname)s - %(process)s:%(thread)s - %(message)s"
+    frmt = u'%(asctime)s - %(levelname)s - %(message)s'
+    loggers = [
+        logging.getLogger(u'beehive'),
+        logging.getLogger(u'beedrones'),
+        logging.getLogger(u'beecell'),
+        logging.getLogger(u'beehive_resource'),
+        logging.getLogger(u'beehive_service'),
+    ]
+    LoggerHelper.file_handler(loggers, logging.DEBUG, log_file, frmt=frmt, formatter=ColorFormatter)
+
+    try:
+        testcase_class.spec_config_file = sys.argv[1]
+    except:
+        pass
+    try:
+        testcase_class.validation_active = str2bool(sys.argv[2])
+    except:
+        pass
+
+    # read external params
+    testcase_class.main_config_file = args.get(u'conf', None)
+    testcase_class.spec_config_file = args.get(u'exconf', None)
+    testcase_class.validation_active = args.get(u'validate', True)
+    testcase_class.run_test_user = args.get(u'user', u'test1')
+
+    from multiprocessing import Process
+    from io import StringIO
+
+    def run_test(index):
+        # run test suite
+        # stream = StringIO()
+        runner = TestRunner(verbosity=2, index=index)
+        testcase_class.index = index
+        runner.run(unittest.TestSuite(map(testcase_class, tests)))
+
+    max_test = 3
+    for i in range(max_test):
+        p = Process(target=run_test, args=(str(i)))
+        p.start()
+
+
+from unittest.result import TestResult as DefaultTestResult
+
+
+class TestResult(DefaultTestResult):
+    """A test result class that can print formatted text results to a stream.
+
+    Used by TextTestRunner.
+    """
+    separator1 = '=' * 70
+    separator2 = '-' * 70
+
+    def __init__(self, stream, descriptions, verbosity, index=0):
+        super(TestResult, self).__init__(stream, descriptions, verbosity)
+        self.stream = stream
+        self.showAll = verbosity > 1
+        self.dots = verbosity == 1
+        self.descriptions = descriptions
+        self.index = index
+
+    def getDescription(self, test):
+        doc_first_line = test.shortDescription()
+        # if self.descriptions and doc_first_line:
+        #     return '\n'.join((str(test), doc_first_line))
+        # else:
+        return str(test)
+
+    # def startTest(self, test):
+    #     # self.stream.write(self.getDescription(test))
+    #     # super(TestResult, self).startTest(test)
+    #     # if self.showAll:
+    #     #     self.stream.write(self.getDescription(test))
+    #     #     self.stream.write(" ... ")
+    #     #     self.stream.flush()
+
+    def addSuccess(self, test):
+        self.stream.write("[%s] " % self.index)
+        self.stream.write(self.getDescription(test))
+        self.stream.write(" ... ")
+        self.stream.write("ok\n")
+        self.stream.flush()
+        # super(TestResult, self).addSuccess(test)
+        # if self.showAll:
+        #     self.stream.writeln("ok")
+        # elif self.dots:
+        #     self.stream.write('.')
+        #     self.stream.flush()
+    #
+    # def addError(self, test, err):
+    #     super(TestResult, self).addError(test, err)
+    #     if self.showAll:
+    #         self.stream.writeln("ERROR")
+    #     elif self.dots:
+    #         self.stream.write('E')
+    #         self.stream.flush()
+    #
+    # def addFailure(self, test, err):
+    #     super(TestResult, self).addFailure(test, err)
+    #     if self.showAll:
+    #         self.stream.writeln("FAIL")
+    #     elif self.dots:
+    #         self.stream.write('F')
+    #         self.stream.flush()
+    #
+    # def addSkip(self, test, reason):
+    #     super(TestResult, self).addSkip(test, reason)
+    #     if self.showAll:
+    #         self.stream.writeln("skipped {0!r}".format(reason))
+    #     elif self.dots:
+    #         self.stream.write("s")
+    #         self.stream.flush()
+    #
+    # def addExpectedFailure(self, test, err):
+    #     super(TestResult, self).addExpectedFailure(test, err)
+    #     if self.showAll:
+    #         self.stream.writeln("expected failure")
+    #     elif self.dots:
+    #         self.stream.write("x")
+    #         self.stream.flush()
+    #
+    # def addUnexpectedSuccess(self, test):
+    #     super(TestResult, self).addUnexpectedSuccess(test)
+    #     if self.showAll:
+    #         self.stream.writeln("unexpected success")
+    #     elif self.dots:
+    #         self.stream.write("u")
+    #         self.stream.flush()
+    #
+    # def printErrors(self):
+    #     if self.dots or self.showAll:
+    #         self.stream.writeln()
+    #     self.printErrorList('ERROR', self.errors)
+    #     self.printErrorList('FAIL', self.failures)
+    #
+    # def printErrorList(self, flavour, errors):
+    #     for test, err in errors:
+    #         self.stream.writeln(self.separator1)
+    #         self.stream.writeln("%s: %s" % (flavour,self.getDescription(test)))
+    #         self.stream.writeln(self.separator2)
+    #         self.stream.writeln("%s" % err)
+
+
+class TestRunner(object):
+    """A test runner class that displays results in textual form.
+
+    It prints out the names of tests as they are run, errors as they
+    occur, and a summary of the results at the end of the test run.
+    """
+    resultclass = TestResult
+
+    def __init__(self, stream=sys.stderr, descriptions=True, verbosity=1,
+                 failfast=False, buffer=False, resultclass=None, index=0):
+        # self.stream = _WritelnDecorator(stream)
+        self.stream = stream
+        self.descriptions = descriptions
+        self.verbosity = verbosity
+        self.failfast = failfast
+        self.buffer = buffer
+        if resultclass is not None:
+            self.resultclass = resultclass
+        self.index = index
+
+    def _makeResult(self):
+        return self.resultclass(self.stream, self.descriptions, self.verbosity, index=self.index)
+
+    def run(self, test):
+        "Run the given test case or test suite."
+        result = self._makeResult()
+        # registerResult(result)
+        result.failfast = self.failfast
+        result.buffer = self.buffer
+        startTime = time.time()
+        startTestRun = getattr(result, 'startTestRun', None)
+        if startTestRun is not None:
+            startTestRun()
+        try:
+            test(result)
+        finally:
+            stopTestRun = getattr(result, 'stopTestRun', None)
+            if stopTestRun is not None:
+                stopTestRun()
+        stopTime = time.time()
+        timeTaken = stopTime - startTime
+        # result.printErrors()
+        # if hasattr(result, 'separator2'):
+        #     self.stream.writeln(result.separator2)
+        # run = result.testsRun
+        # self.stream.writeln("Ran %d test%s in %.3fs" %
+        #                     (run, run != 1 and "s" or "", timeTaken))
+        # self.stream.writeln()
+        #
+        # expectedFails = unexpectedSuccesses = skipped = 0
+        # try:
+        #     results = map(len, (result.expectedFailures,
+        #                         result.unexpectedSuccesses,
+        #                         result.skipped))
+        # except AttributeError:
+        #     pass
+        # else:
+        #     expectedFails, unexpectedSuccesses, skipped = results
+        #
+        # infos = []
+        # if not result.wasSuccessful():
+        #     self.stream.write("FAILED")
+        #     failed, errored = map(len, (result.failures, result.errors))
+        #     if failed:
+        #         infos.append("failures=%d" % failed)
+        #     if errored:
+        #         infos.append("errors=%d" % errored)
+        # else:
+        #     self.stream.write("OK")
+        # if skipped:
+        #     infos.append("skipped=%d" % skipped)
+        # if expectedFails:
+        #     infos.append("expected failures=%d" % expectedFails)
+        # if unexpectedSuccesses:
+        #     infos.append("unexpected successes=%d" % unexpectedSuccesses)
+        # if infos:
+        #     self.stream.writeln(" (%s)" % (", ".join(infos),))
+        # else:
+        #     self.stream.write("\n")
+        return result
