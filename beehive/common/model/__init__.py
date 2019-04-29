b8cbfc6c6cc106c6513182a99898ee15f90cff8b
Antonio <a.brasile@tdnet.it>
2019-04-29 15:11:54 +0200

    bypass bulk_save con add_all
    reverted

--- beehive/common/model/__init__.py
+++ beehive/common/model/__init__.py
@@ -962,10 +962,9 @@ class AbstractDbManager(object):
             if isinstance(entity, BaseEntity):
                 entity.modification_date = datetime.today()
         
-        self.add_all(entities)
-        #session = self.get_session()
-        #session.bulk_save_objects(entities)
-        #session.flush()
+        session = self.get_session()
+        session.bulk_save_objects(entities)
+        session.flush()
         self.logger.info(u'Bulk updated')
         return entities   
 
