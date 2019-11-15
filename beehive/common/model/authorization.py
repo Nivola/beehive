1cdc4bc0ecb109d013a503a3232add09799fc216
1362 <sergio.tonani@csi.it>
2019-11-15 11:31:06 +0100

    test console.py

--- beehive/common/model/authorization.py
+++ beehive/common/model/authorization.py
@@ -192,6 +192,8 @@ class User(Base, BaseEntity):
         if expiry_date is None:
             expiry_date = datetime.datetime.today()+datetime.timedelta(days=365)
         self.expiry_date = expiry_date
+        
+        password = password.encode('utf-8')
         if password is not None:
             # generate new salt, and hash a password 
             # self.password = sha256_crypt.encrypt(password)
