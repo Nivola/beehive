c5ff6475789b14e75edf1956624dd199a60e8254
1362 <sergio.tonani@csi.it>
2019-11-15 10:35:09 +0100

    rimossa dir build

--- build/lib.linux-x86_64-2.7/beehive/module/auth/views/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
-# (C) Copyright 2018-2019 CSI-Piemonte
-
-from .auhtorization import AuthorizationAPI
-from .keyauth import KeyAuthApi
-from .simplehttp import SimpleHttpAuthApi
