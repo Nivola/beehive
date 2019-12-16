7c64d45d55a8123ec4f03a24861f87917570837b
1362 <sergio.tonani@csi.it>
2019-12-16 15:24:01 +0100

    modificato file auth.yml e parsing

--- beehive/common/flask_app.py
+++ beehive/common/flask_app.py
@@ -126,7 +126,7 @@ class BeehiveApp(Flask):
         logname = uwsgi_util.opt['api_id']
 
         # base logging
-        file_name = self.log_path + logname + '.log'
+        file_name = '%s/%s.log' % (self.log_path, logname)
         file_name = file_name.decode('utf-8')
         loggers = [
             logger,
