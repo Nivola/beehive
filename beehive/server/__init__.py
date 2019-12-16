fa10a5a61656ae539e9f93e22ec4f32deba12d59
1362 <sergio.tonani@csi.it>
2019-12-16 18:15:19 +0100

    modificato file auth.yml e parsing

--- beehive/server/__init__.py
+++ beehive/server/__init__.py
@@ -6,30 +6,43 @@
 from sys import argv
 from collections import OrderedDict
 
+from beecell.simple import read_file
+
 
 def configure_server():
     # config_file = argv[1:][1]
     config_file = argv[1]
 
-    from six.moves.configparser import RawConfigParser
+    # from six.moves.configparser import RawConfigParser
+    #
+    # class MultiOrderedDict(OrderedDict):
+    #     def __setitem__(self, key, value):
+    #         if isinstance(value, list) and key in self:
+    #             self[key].extend(value)
+    #         else:
+    #             super(MultiOrderedDict, self).__setitem__(key, value)
+    #
+    # config = RawConfigParser(dict_type=MultiOrderedDict)
+    # config.read(config_file)
 
-    class MultiOrderedDict(OrderedDict):
-        def __setitem__(self, key, value):
-            if isinstance(value, list) and key in self:
-                self[key].extend(value)
-            else:
-                super(MultiOrderedDict, self).__setitem__(key, value)
+    # params = {i[0]: i[1] for i in config.items('uwsgi')}
+    # params['task_module'] = params.get('task_module', '').split('\n')
+    # params['api_module'] = params.get('api_module', '').split('\n')
+    # params['api_plugin'] = params.get('api_plugin', '').split('\n')
+    # params['event_handler'] = params.get('event_handler', '').split('\n')
 
-    config = RawConfigParser(dict_type=MultiOrderedDict)
-    config.read(config_file)
+    params = read_file(config_file)
 
-    params = {i[0]: i[1] for i in config.items('uwsgi')}
-    params['task_module'] = params.get('task_module', '').split('\n')
-    params['api_module'] = params.get('api_module', '').split('\n')
-    params['api_plugin'] = params.get('api_plugin', '').split('\n')
-    params['event_handler'] = params.get('event_handler', '').split('\n')
+    fields = ['api_module']
+    for field in fields:
+        num = int(params.pop(field, 0))
+        if num > 0:
+            params[field] = []
+            for i in range(1, num):
+                item = params.get('%s.%s' % (field, i)).decode('utf-8')
+                params[field].append(item)
 
-    import beecell.server.gevent_ssl
+    # import beecell.server.gevent_ssl
     from gevent import monkey
     monkey.patch_all()
 
