327d8d4e83ea8c6a2ec45c839b88b8c7aca01fa9
1362 <sergio.tonani@csi.it>
2019-12-16 18:38:31 +0100

    modificato file auth.yml e parsing

--- beehive/server/task.py
+++ beehive/server/task.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
 
     from beehive.server import configure_server
     params = configure_server()
-
+    print(params)
     # from beehive.common.task_v2.manager import start_task_manager
     from beehive.common.task.manager import start_task_manager
     start_task_manager(params)
